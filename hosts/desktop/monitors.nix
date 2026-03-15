a2dccb798a6f8bd96cb40257f19c28f27ee5fe59
Hangover64 <hendrikfarnkopf@pm.me>
2026-03-15 19:48:08 +0100

    monitors pc

--- hosts/desktop/monitors.nix
+++ hosts/desktop/monitors.nix
@@ -7,5 +7,10 @@
     "DP-1,1920x1080@144,1080x0,1"
     "DP-2,2560x1440@240,3000x0,1"
     ];
+    workspace[
+    "1, monitor:DP-2, default:true"
+    "2, monitor:DP-1"
+    "3, monitor:DP-3"
+    ]
   };
 }
