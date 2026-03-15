abe1f5dda5b35c661f48e82a0f548c8f4f79a51f
Hangover64 <hendrikfarnkopf@pm.me>
2026-03-15 19:24:51 +0100

    monitors pc

---
+++ hosts/desktop/monitors.nix
@@ -0,0 +1,11 @@
+{ ... }:
+
+{
+  wayland.windowManager.hyprland.settings = {
+    monitor = [
+    "DP-3,1920x1080@144,0x0,1,transform,1"
+    "DP-1,1920x1080@144,1080x0,1"
+    "DP-2,2560x1440@240,3000x0,1"
+    ];
+  };
+}
