7b97b014cdf5cbd27cfd75d077c6ef6453cf0448
Hangover64 <hendrikfarnkopf@pm.me>
2026-03-22 23:21:31 +0100

    monitor res change

--- hosts/laptop/monitors.nix
+++ hosts/laptop/monitors.nix
@@ -1,6 +1,8 @@
-{ ... }:
+{ lib, ... }:
 {
-  wayland.windowManager.hyprland.settings.monitor = [
+  wayland.windowManager.hyprland.settings = {
+    monitor = lib.mkForce [
   "eDP-1, 2880x1800@120, 0x0, 1"
-  ];
+    ];
+  };
 }
