e3a2e0edfc050fc52f1b6206d49e616a1f3e3d7c
Hangover64 <hendrikfarnkopf@pm.me>
2026-03-30 11:23:17 +0200

    monitor config laptop+arbeit

--- hosts/laptop/monitors.nix
+++ hosts/laptop/monitors.nix
@@ -2,9 +2,9 @@
 {
   wayland.windowManager.hyprland.settings = {
     monitor =  [
-  "eDP-1, 2880x1800@120, 3840x0, 1.5"
-  "DP-6, 1920x1080@60, 1920x0, 1.5"
-  "DP-7, 1920x1080@60, 0x0, 1.5"
+  "eDP-1, 2880x1800@120, 3840x0, 1,5"
+  "DP-6, 1920x1080@60, 1920x0, 1"
+  "DP-7, 1920x1080@60, 0x0, 1"
 
     ];
     workspace = [
