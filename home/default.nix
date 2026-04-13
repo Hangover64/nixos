e8681ce4e6d1ee5d8440a3fea032ee10708f7ff2
Hangover64 <hendrikfarnkopf@pm.me>
2026-04-13 11:33:19 +0200

    caelestia cli missing

--- home/default.nix
+++ home/default.nix
@@ -5,7 +5,6 @@
 
   imports = [
     inputs.caelestia-shell.homeManagerModules.default
-    inputs.caelestia-shell.inputs.caelestia-cli.packages.x86_64-linux.default
     ./hyprland.nix
     ./hypr-extras.nix
     ../hosts/laptop/monitors.nix
