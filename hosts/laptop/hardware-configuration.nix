08c5b40dbb34a9bdd2634e644f3d488f399157fc
Hangover64 <hendrikfarnkopf@pm.me>
2026-04-13 14:54:29 +0200

    stdenv.hostPlatform

--- hosts/laptop/hardware-configuration.nix
+++ hosts/laptop/hardware-configuration.nix
@@ -26,6 +26,6 @@
 
   swapDevices = [ ];
 
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  nixpkgs.stdenv.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
