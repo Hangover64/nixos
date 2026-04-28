616a3159294f88bb93bce14961b3ef3fa685babd
Hangover64 <hendrikfarnkopf@pm.me>
2026-04-28 18:15:09 +0200

    virtualbox

--- modules/common.nix
+++ modules/common.nix
@@ -42,6 +42,7 @@
   };
 
   virtualisation.virtualbox.host.enable = true;
+  virtualisation.virtualbox.host.enableKvm = true;
   users.extraGroups.vboxusers.members = [ "hendrikf" ];
 
   home-manager = {
