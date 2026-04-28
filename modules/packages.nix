68a754760e162a4a90783d318ef9c3ed59a9ba3f
Hangover64 <hendrikfarnkopf@pm.me>
2026-04-28 11:50:57 +0200

    monado remove

--- modules/packages.nix
+++ modules/packages.nix
@@ -3,12 +3,6 @@
   programs.firefox.enable = true;
   programs.steam.enable = true;
 
-
-  services.monado = {
-    enable = true;
-    defaultRuntime = true;
-  };
-
   environment.systemPackages = with pkgs; [
 # CLI tools
       wget
