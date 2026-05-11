1f2af751be26de2fe292733ddcf907dc001543d5
Hangover64 <hendrikfarnkopf@pm.me>
2026-05-11 10:53:12 +0200

    nixCitizen

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -23,6 +23,7 @@
   };
 
   nix.settings = {
+    trusted-users = [ "hendrikf" ];
         substituters = ["https://nix-citizen.cachix.org"];
         trusted-public-keys = ["nix-citizen.cachix.org-1:lPMkWc2X8XD4/7YPEEwXKKBg+SVbYTVrAaLA2wQTKCo="];
     };
