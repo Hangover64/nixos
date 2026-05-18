7d3e59fa0fe22d007b8e279701668376c0562ba3
Hangover64 <hendrikfarnkopf@pm.me>
2026-05-18 11:53:59 +0200

    lualang

--- hosts/laptop/monitors.nix
+++ hosts/laptop/monitors.nix
@@ -2,14 +2,7 @@
 {
   wayland.windowManager.hyprland.settings = {
     monitor =  [
-  "DP-6, 1920x1080@60, 0x0, 1"
-  "DP-7, 1920x1080@60, 1920x0, 1"
   "eDP-1, 2880x1800@120, 3840x0, 1.5"
-    ];
-    workspace = [
-    "1, monitor:eDP-1, default:true"
-    "2, monitor:DP-7"
-    "3, monitor:DP-6"
     ];
   };
 }
