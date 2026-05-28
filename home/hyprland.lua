f28107a219958162f2146da6dae06a4e86f66159
Hangover64 <hendrikfarnkopf@pm.me>
2026-05-28 08:07:06 +0200

    hyprland gestures

--- home/hyprland.lua
+++ home/hyprland.lua
@@ -220,6 +220,13 @@ hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = tr
 hl.bind("XF86AudioPlay",  hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
 hl.bind("XF86AudioPrev",  hl.dsp.exec_cmd("playerctl previous"),   { locked = true })
 
+
+-- Gestures
+hl.gesture({ fingers = 3, direction = "horizontal", action = "workspace" })
+hl.gesture({ fingers = 3, direction = "vertical", action = "resize"}
+)
+
+
 -- =============================================================================
 -- Window Rules
 -- =============================================================================
