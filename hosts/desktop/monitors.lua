c3942afcda3045bd8f29e57f1643d7c44801d183
Hangover64 <hendrikfarnkopf@pm.me>
2026-06-06 11:07:19 +0200

    monitor pc

--- hosts/desktop/monitors.lua
+++ hosts/desktop/monitors.lua
@@ -1,3 +1,3 @@
 hl.monitor({ output = "DP-1", mode = "2560x1440@240", position = "3000x0", scale = 1 })
-hl.monitor({ output = "DP-2", mode = "1920x1080@144", position = "1080x500", scale = 1 })
+hl.monitor({ output = "DP-2", mode = "1920x1080@144", position = "1080x300", scale = 1 })
 hl.monitor({ output = "DP-3", mode = "1920x1080@144", position = "0x0", scale = 1, transform = 3 })
