daf28c1fbf0c44cb95857271a4f3c99c471f801f
Hangover64 <hendrikfarnkopf@pm.me>
2026-06-21 20:43:27 +0200

    caelestiacli

--- flake.nix
+++ flake.nix
@@ -6,6 +6,10 @@
 
     caelestia-shell = {
       url = "github:caelestia-dots/shell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    caelestia-cli = {
       url = "github:caelestia-dots/cli";
       inputs.nixpkgs.follows = "nixpkgs";
     };
